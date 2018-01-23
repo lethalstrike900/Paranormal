58e33ab6e19bca58733824583811a712b4bc8dc6
john allan lee <lethalstrike.lee@gmail.com>
2018-01-23 15:08:33 -0800

    general bug fixes and changed
    added the pick up function

--- Paranormal/Assets/PlayerLookController.cs
+++ Paranormal/Assets/PlayerLookController.cs
@@ -8,13 +8,17 @@ public class PlayerLookController : MonoBehaviour {
     Vector2 smoothV;
     public float sensitivity = 5.0f;
     public float smoothing = 2f;
-
+    private float rotationY = 0f;
+    float max = 60.0f;
+    float min = -45.0f;
     GameObject character;
-
+    Vector3 v3Rotate = Vector3.zero;
+   
 
     void Start () {
         character = this.transform.parent.gameObject;
-      }
+        
+    }
      
      void Update () {
         var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
@@ -27,6 +31,16 @@ public class PlayerLookController : MonoBehaviour {
         transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
         character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
 
+        LockRotateY();
      }
+
+    void LockRotateY()
+    {
+        rotationY += Input.GetAxis("Mouse Y") * sensitivity;
+        rotationY = Mathf.Clamp(rotationY, min, max);
+
+        transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+
+    }
 	 
 }
